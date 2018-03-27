87ce0c8ffe7a246b352bfac09b29b636ee8772bf
kashish khullar <kkhullar7@gmail.com>
2018-03-27 23:35:24 +0530

    commented response in registration controller of citizen

--- app/controllers/api/v1/citizen/registrations_controller.rb
+++ app/controllers/api/v1/citizen/registrations_controller.rb
@@ -16,7 +16,8 @@ class Api::V1::Citizen::RegistrationsController < ApplicationController
 
 		@citizen = Citizen.new(citizen_params)
 		if(@citizen.save)
-			render json: {status:"SUCCESS",message: "Registration Successful",data: @citizen.as_json(only: [:email,:mobile,:authentication_token,:first_name,:last_name,:dob,:aadhar_no,:dlnumber,:address,:pincode])},status: :created
+			render json: {status:"SUCCESS",message: "Registration Successful",data: :true},status: :created
+			# render json: {status:"SUCCESS",message: "Registration Successful",data: @citizen.as_json(only: [:email,:mobile,:authentication_token,:first_name,:last_name,:dob,:aadhar_no,:dlnumber,:address,:pincode])},status: :created
 		else
 			render json: {status:"ERROR",message: "Registration Failed",data: :false},status: :unauthorized
 		end
